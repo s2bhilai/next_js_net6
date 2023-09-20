383da7f805ef82fdb68fbc2ef6140ac63cab23be
Subin <subinbs4a@gmail.com>
2023-09-20 12:02:53 +0300

    Added search service

---
+++ Carsties/Carsties/src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+    public class AuctionSvcHttpClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly IConfiguration configuration;
+
+        public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            this.httpClient = httpClient;
+            this.configuration = configuration;
+        }
+
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            var lastUpdated = await DB.Find<Item, string>()
+                .Sort(x => x.Descending(x => x.UpdatedAt))
+                .Project(x => x.UpdatedAt.ToString())
+                .ExecuteFirstAsync();
+
+            return await httpClient.GetFromJsonAsync<List<Item>>(configuration["AuctionServiceUrl"]
+                + "/api/auctions?date=" + lastUpdated);
+        }
+    }
+}
