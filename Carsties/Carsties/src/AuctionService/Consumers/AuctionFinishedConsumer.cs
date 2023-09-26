cc71c8a7d9cf325ecdaa919a43818520e21429c9
Subin <subinbs4a@gmail.com>
2023-09-26 12:28:10 +0300

    Added YARP Gateway

---
+++ Carsties/Carsties/src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext _auctionDbContext;
+
+        public AuctionFinishedConsumer(AuctionDbContext auctionDbContext)
+        {
+            this._auctionDbContext = auctionDbContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("--> Consuming auction finished");
+
+            var auction = await _auctionDbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if(context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice ? 
+                Entities.Status.Finished : Entities.Status.ReserveNotMet;
+
+            await _auctionDbContext.SaveChangesAsync();
+        }
+    }
+}
