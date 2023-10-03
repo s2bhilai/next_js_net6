92eba211ff5b1ad4686c2da02498b9b6cf18324b
Subin <subinbs4a@gmail.com>
2023-10-03 12:00:04 +0300

    added filtering logic, order by

--- Carsties/Carsties/frontend/web-app/app/auctions/Filters.tsx
+++ Carsties/Carsties/frontend/web-app/app/auctions/Filters.tsx
@@ -1,31 +1,106 @@
+import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button } from 'flowbite-react';
-import ButtonGroup from 'flowbite-react/lib/esm/components/Button/ButtonGroup';
 import React from 'react'
-
-type Props = {
-    pageSize: number;
-    setPageSize: (size: number) => void;
-}
+import { AiOutlineClockCircle, AiOutlineSortAscending } from 'react-icons/ai';
+import { BsFillStopCircleFill, BsStopwatchFill } from 'react-icons/bs';
+import { GiFinishLine, GiFlame } from 'react-icons/gi';
 
 const pageSizeButtons = [4,8,12];
 
-export default function Filters({pageSize,setPageSize}: Props) {
+const orderButtons = [
+    {
+        label: 'Alphabetical',
+        icon: AiOutlineSortAscending,
+        value: 'make'
+    },
+    {
+        label: 'End date',
+        icon: AiOutlineClockCircle,
+        value: 'endingSoon'
+    },
+    {
+        label: 'Recently added',
+        icon: BsFillStopCircleFill,
+        value: 'new'
+    }
+]
+
+const filterButtons = [
+    {
+        label: 'Live Auctions',
+        icon: GiFlame,
+        value: 'live'
+    },
+    {
+        label: 'Ending < 6 hours',
+        icon: GiFinishLine,
+        value: 'endingSoon'
+    },
+    {
+        label: 'Completed',
+        icon: BsStopwatchFill,
+        value: 'finished'
+    }
+]
+
+export default function Filters() {
+  const pageSize = useParamsStore(state => state.pageSize);
+  const setParams = useParamsStore(state => state.setParams);
+  const orderBy = useParamsStore(state => state.orderBy);
+  const filterBy = useParamsStore(state => state.filterBy);
+
   return (
     <div className='flex justify-between items-center mb-4'>
+
+        <div>
+            <span className='uppercase text-sm text-gray-500 mr-2'>Filter by</span>
+            <Button.Group>
+                {filterButtons.map(({label, icon: Icon,value}) => {
+                    return (
+                        <Button key={value} 
+                                onClick={() => setParams({ filterBy: value }) }
+                                color={`${filterBy === value ? 'red' : 'gray'}`}
+                             >
+                            <Icon className='mr-3 h-4 w-4'  />
+                            {label}
+                        </Button>
+                    );
+                } )}
+            </Button.Group>
+        </div>
+
+        <div>
+            <span className='uppercase text-sm text-gray-500 mr-2'>Order by</span>
+            <Button.Group>
+                {orderButtons.map(({label, icon: Icon,value}) => {
+                    return (
+                        <Button key={value} 
+                                onClick={() => setParams({ orderBy: value }) }
+                                color={`${orderBy === value ? 'red' : 'gray'}`}
+                             >
+                            <Icon className='mr-3 h-4 w-4'  />
+                            {label}
+                        </Button>
+                    );
+                } )}
+            </Button.Group>
+        </div>
+
         <div>
             <span className='uppercase text-sm text-gray-500 mr-2'>Page Size</span>
-            <ButtonGroup>
+            <Button.Group>
                 {pageSizeButtons.map((v,index) => {
                     return (
-                        <Button key={index} onClick={() => setPageSize(v)}
+                        <Button key={index} onClick={() => setParams({ pageSize: v })}
                           color={ `${pageSize === v ? 'red' : 'gray'}` }
                           className='focus:ring-0'>
                             {v}
                         </Button>
                     );
                 })}
-            </ButtonGroup>
+            </Button.Group>
         </div>
+        
     </div>
   )
 }
--- Carsties/Carsties/frontend/web-app/app/auctions/Listings.tsx
+++ Carsties/Carsties/frontend/web-app/app/auctions/Listings.tsx
@@ -3,37 +3,59 @@
 
 import React, { useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
-import { Auction } from '@/types';
+import { Auction, PagedResult } from '@/types';
 import AppPagination from '../components/AppPagination';
 import { getData } from '../actions/auctionActions';
 import Filters from './Filters';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { shallow } from 'zustand/shallow';
+import queryString from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
-  const [auctions,setAuctions] = useState<Auction[]>([]);
-  const [pageCount, setPageCount] = useState(0);
-  const [pageNumber,setPageNumber] = useState(1);
-  const [pageSize, setPageSize] = useState(4);
+  const [data,setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(state => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy
+  }),shallow);
+
+  const setParams = useParamsStore(state => state.setParams);
+  const url = queryString.stringifyUrl({url: '',query: params});
+
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber: pageNumber });
+  }
 
   useEffect(() => {
-    getData(pageNumber,pageSize).then(data => {
-      setAuctions(data.results);
-      setPageCount(data.pageCount);
+    getData(url).then(data => {
+      setData(data);
     });
-  },[pageNumber,pageSize]);
+  },[url]);
 
-  if(auctions.length === 0) return <h3>Loading....</h3>;
+  if(!data) return <h3>Loading....</h3>;
 
   return (
     <>
-      <Filters pageSize={pageSize} setPageSize={setPageSize} />
-      <div className='grid grid-cols-4 gap-6'>
-          {auctions.map((auction) => (
-              <AuctionCard auction={auction} key={auction.id} />
+      <Filters />
+      { data.totalCount === 0 ? (
+        <EmptyFilter showReset />
+      ): (
+        <>
+         <div className='grid grid-cols-4 gap-6'>
+          {data.results.map((auction) => (
+                <AuctionCard auction={auction} key={auction.id} />
           ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <AppPagination pageChanged={setPageNumber} currentPage={pageNumber} pageCount={pageCount} />
-      </div>
+         </div>
+         <div className='flex justify-center mt-4'>
+            <AppPagination pageChanged={setPageNumber} 
+               currentPage={params.pageNumber} pageCount={data.pageCount} />
+         </div>
+        </>
+      ) }
+      
     </>
   )
 }
