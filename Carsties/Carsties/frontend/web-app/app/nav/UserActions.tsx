f636269befc33d1254d4fb0a282a40fd3e0112ea
Subin <subinbs4a@gmail.com>
2023-10-05 11:24:59 +0300

    Added NextAuth.js for authentication with nextjs

---
+++ Carsties/Carsties/frontend/web-app/app/nav/UserActions.tsx
@@ -0,0 +1,48 @@
+
+'use client'
+
+import { Dropdown } from 'flowbite-react'
+import React from 'react'
+import Link from 'next/link'
+import { User } from 'next-auth'
+import { HiCog, HiUser } from 'react-icons/hi2'
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
+import { signOut } from 'next-auth/react'
+
+type Props = {
+  user: Partial<User>
+}
+
+export default function UserActions({user}: Props) {
+  return (
+      <Dropdown
+        inline
+        label={`Welcome ${user.name}`}
+      >
+        <Dropdown.Item icon={HiUser}>
+          <Link href='/'>
+            My Auctions
+          </Link>
+        </Dropdown.Item>
+        <Dropdown.Item icon={AiFillTrophy}>
+          <Link href='/'>
+            Auctions won
+          </Link>
+        </Dropdown.Item>
+        <Dropdown.Item icon={AiFillCar}>
+          <Link href='/'>
+            Sell my car
+          </Link>
+        </Dropdown.Item>
+        <Dropdown.Item icon={HiCog}>
+          <Link href='/session'>
+            Session (dev only)
+          </Link>
+        </Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item icon={AiOutlineLogout} onClick={() => signOut({ callbackUrl: '/' })}>
+          Sign Out
+        </Dropdown.Item>
+      </Dropdown>
+  )
+}
---
+++ Carsties/Carsties/frontend/web-app/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import { DefaultSession } from 'next-auth'
+
+declare module "next-auth" {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+      user: {
+        username: string
+      } & DefaultSession['user']
+    }
+
+    interface Profile {
+        username: string
+    }
+  }
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        username: string
+        access_token?: string
+    }
+}
