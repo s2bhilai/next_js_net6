a7fc358e309c78fba556ea5130ba1bfe4eef8e15
Subin <subinbs4a@gmail.com>
2023-10-15 08:27:33 +0300

    Added SignalR notification service and updated docker compose file

--- Carsties/Carsties/src/GatewayService/Program.cs
+++ Carsties/Carsties/src/GatewayService/Program.cs
@@ -20,8 +20,19 @@ namespace GatewayService
                     options.TokenValidationParameters.NameClaimType = "username";
                 });
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("customPolicy", b =>
+                {
+                    b.AllowAnyHeader()
+                        .AllowAnyMethod().AllowAnyMethod().AllowCredentials()
+                        .WithOrigins(builder.Configuration["ClientApp"]);
+                });
+            });
 
             var app = builder.Build();
+
+            app.UseCors();
 
             app.MapReverseProxy();
 
---
+++ Carsties/Carsties/src/NotificationService/Program.cs
@@ -0,0 +1,33 @@
+using MassTransit;
+using NotificationService.Consumers;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    //Pick any one consumer, mass transit will create queue for others
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
